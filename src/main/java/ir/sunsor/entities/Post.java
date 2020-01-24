86e39d1dd2fcd2d67adf61c6264bfee11f34af29
asus <hassan.shokoohimatin@gmail.com>
2020-01-25 01:21:05 +0330

    post as an entity added

--- src/main/java/ir/sunsor/entities/Post.java
+++ src/main/java/ir/sunsor/entities/Post.java
@@ -1,4 +1,38 @@
 package ir.sunsor.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data @AllArgsConstructor @NoArgsConstructor @Table @Entity
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private String type;
+    private String time;
+    private int likesCount;
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Post{" + id + "}{" +
+                " title= " + title +
+                " type= " + type +
+                " time= " + time +"( " + likesCount + "likes" + " )" +
+                " user= " + user.getUsername() + "}";
+    }
 }
